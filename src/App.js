a906415ee8978004e64acd0ee77bcc00b9a0ef8e
Fuad Chonora <fuadchonora@gmail.com>
2022-03-02 15:10:24 +0530

    Minor UI Improvements

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 import './App.css';
 import { Typography } from '@mui/material';
 import { words } from './words';
+
+const modalStyle = {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	backgroundColor: 'white',
+	boxShadow: 24,
+	padding: '20px',
+	borderRadius: '18px',
+};
 
 const style = { width: '70px' };
 
 const idxs = [0, 1, 2, 3, 4];
 
 function App() {
+	const [open, setOpen] = React.useState(false);
+
 	const [inputState, setInputState] = React.useState({
 		0: { letter: '', color: 0 },
 		1: { letter: '', color: 0 },
@@ -57,9 +73,9 @@ function App() {
 		//search words
 		const results = [];
 
-		console.log(bads);
-		console.log(goods);
-		console.log(corrects);
+		// console.log(bads);
+		// console.log(goods);
+		// console.log(corrects);
 
 		for (let word of foundWords) {
 			// check if the word has some of the bad letters
@@ -77,6 +93,7 @@ function App() {
 
 		// console.log(results);
 		setFoundWords(results);
+		handleOpen();
 
 		//add the word to checks with color
 		const newCheck = {
@@ -94,27 +111,42 @@ function App() {
 		console.log(word);
 		idxs.map((i) => {
 			newInputState[i].letter = word[i];
-			newInputState[i].color = 0;
+			newInputState[i].color = newInputState[i].color === 2 ? 2 : 0;
 			return true;
 		});
 		setInputState(newInputState);
-		console.log(inputState);
+		handleClose();
 	};
+
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
 	return (
 		<div className="App">
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box style={modalStyle}>
+					<Typography variant="h6" component="h2">
+						Possible Words
+					</Typography>
+					<Typography sx={{ mt: 2 }}></Typography>
+					{foundWords.length > 0 && foundWords.length < words.length ? (
+						foundWords.map((foundWord) => (
+							<Typography sx={{ mt: 1 }} key={Math.random()} onClick={() => addToInput(foundWord)}>
+								{foundWord}
+							</Typography>
+						))
+					) : (
+						<Typography sx={{ mt: 1 }}>Oops! No Words!</Typography>
+					)}
+				</Box>
+			</Modal>
 			<header className="App-header">
-				<Stack direction="row" spacing={20}>
-					<Stack direction="column">
-						<Typography>Next Round Suggestions</Typography>
-						{foundWords.length !== words.length &&
-							foundWords.map((foundWord) => (
-								<Typography key={Math.random()} onClick={() => addToInput(foundWord)}>
-									{foundWord}
-								</Typography>
-							))}
-					</Stack>
-
+				<Stack direction="column" spacing={20}>
 					<Stack direction="column" spacing={2}>
 						<Stack direction="row" spacing={2}>
 							{idxs.map((i) => (
@@ -143,8 +175,11 @@ function App() {
 							))}
 						</Stack>
 
-						<Stack direction="row" spacing={2}>
-							<Button id="button-next" variant="outlined" style={style} onClick={handleNextClick}>
+						<Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+							<Button disabled={foundWords.length === words.length} variant="outlined" onClick={handleOpen}>
+								Possible Words
+							</Button>
+							<Button variant="outlined" onClick={handleNextClick}>
 								Next
 							</Button>
 						</Stack>
